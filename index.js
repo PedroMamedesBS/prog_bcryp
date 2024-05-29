c1f4ae29258b05bca860de7743ea0d5a3e1bcc1e
PEDRO MAMEDES BRAGHINE DA SILVA <pedro_mb_silva@estudante.sesisenai.org.br>
2024-05-29 08:40:19 -0300

    Finalizado o Front-end

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const cors = require('cors')
 const bcrypt = require('bcrypt')
 const conn = require('./db/conn')
-const Usuario = require('./modules/Usuario')
+const Usuario = require('./model/Usuario')
 
 const PORT = 3000
 const hostname = 'localhost'
@@ -13,6 +13,39 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cors())
 /*------------------------------------------------ */
+
+app.post('/logar', async (req, res)=> {
+    const login = req.body
+    console.log(login)
+    try {
+        const pesq = await Usuario.findOne({ where: { email: login.email }, raw: true })
+        console.log(pesq)
+
+        if (pesq === null) {
+            console.log('Usuário não encontrado no banco de dados!')
+            res.status(404).json({message: 'Usuário não encontrado no banco de dados' })
+        } else if (pesq.email == login.email) {
+
+            bcrypt.compare(login.senha, pesq.senha, (err, result)=> {
+                if (err) {
+                    console.log("Erro ao verificar a criptografia!")
+                    res.status(500).json({ message: "Erro ao verificar a criptografia!" })
+                } else if (result) {
+                    console.log("Senha correta!")
+                    res.status(200).json({ message: 'Login feito com sucesso!' })
+                } else {
+                    console.log("Senha incorreta")
+                    res.status(404).json({ message: "Senha incorreta. Tente novamente!" })
+                }
+            })
+        }
+
+    } catch(err) {
+        console.error('Erro ao consultar o usuário no banco de dados',err)
+        res.status(500).json({ message: 'Erro ao consultar o usuário no banco de dados' })
+    }
+
+})
 
 app.post('/cadastrar', (req, res) => {
     const cad = req.body
@@ -25,11 +58,11 @@ app.post('/cadastrar', (req, res) => {
         }
         try {
             const grav = await Usuario.create(
-                {nome: cad.nome, email: cad.email, senha: hash })
-            
+                { nome: cad.nome, email: cad.email, senha: hash })
+
             console.log(grav)
             res.status(200).json(grav)
-            
+
         } catch (err) {
             console.error("Erro ao gravaros dados!", err)
             res.status(500).json({ message: "Erro ao gravar os dados!" })
